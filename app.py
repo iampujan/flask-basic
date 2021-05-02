dbcd4d50ebe000487680ad85e3f84d756a8ca287
Pujan Thapa <iampujan@outlook.com>
2021-05-02 14:43:41 +0545

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def main():
+    return "Welcome to TechCraft Assignment"
+
 @app.route('/<name>')
 def main(name):
     return "Hello" + " " + name
